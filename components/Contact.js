8ff951250728f5166c82497cc1614f35a6ef229b
Daniel Balcer <kemaron@gmail.com>
2019-04-18 10:37:07 +0200

    Refactor Contact.js

--- components/Contact.js
+++ components/Contact.js
@@ -5,17 +5,18 @@ var Contact = React.createClass({
   
     render: function() {
       return (
-        React.createElement('div', {className: 'contactItem'},
-          React.createElement('img', {
-            className: 'contactImage',
-            src: 'https://cdn1.iconfinder.com/data/icons/ios-11-glyphs/30/contact_card-128.png'
-          }),
-          React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
-          React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
-          React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email},
-            this.props.item.email
-          )
-        )
+        <div className = {'contactItem'}>
+          <img className = {'contactImage'} src={'https://cdn1.iconfinder.com/data/icons/ios-11-glyphs/30/contact_card-128.png'}/>
+          <p className = {'contactLabel'}>
+            Imię: {this.props.item.firstName}
+          </p>
+          <p className = {'contactLabel'}>
+            Nazwisko: {this.props.item.lastName}
+          </p>
+          <a className = {'contactEmail'} href={'mailto:' + this.props.item.email}>
+            {this.props.item.email}
+          </a>          
+        </div>
       )
     },
   });
